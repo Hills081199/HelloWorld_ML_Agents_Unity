c5b66a105511be778a1e364911367923921147ab
hieulenovo <hieubait99@gmail.com>
2024-05-09 17:37:29 +0700

    hello world project with ml agents in unity, move object to target object

---
+++ Assets/Scripts/MoveToTargetAgents.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToTargetAgents : Agent
+{
+
+    public Transform target;
+    public SpriteRenderer backgroundSpriteRenderer;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-3.5f, -1.5f), Random.Range(-3.5f, 3.5f));
+        target.localPosition = new Vector3(Random.Range(1.5f, 3.5f), Random.Range(-3.5f, 3.5f));
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation((Vector2)transform.localPosition);
+        sensor.AddObservation((Vector2)target.localPosition);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+
+        float movementSpeed = 5f;
+        transform.localPosition += new Vector3(moveX, moveY) * Time.deltaTime * movementSpeed;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            backgroundSpriteRenderer.color = Color.green;
+            EndEpisode();
+        }
+        else if(collision.TryGetComponent(out Walls walls))
+        {
+            AddReward(-2f);
+            backgroundSpriteRenderer.color = Color.red;
+            EndEpisode();
+        }
+    }
+}
+
